2fbbaa35489d57f5f94f9862481aaccb48a7b150
Nazar Rusnak <inazarrusnak@gmail.com>
2026-03-11 21:25:37 +0200

    Migrate to the new DataStore.Builder api, implement local-storage private bindings

---
+++ core/local-storage/impl/src/commonMain/kotlin/org/example/project/core/local/storage/di/DataStoreScope.kt
@@ -0,0 +1,3 @@
+package org.example.project.core.local.storage.di
+
+internal abstract class DataStoreScope private constructor()
--- core/local-storage/impl/src/jvmMain/kotlin/org/example/project/core/local/storage/DataStoreGraph.kt
+++ core/local-storage/impl/src/jvmMain/kotlin/org/example/project/core/local/storage/DataStoreGraph.kt
@@ -1,36 +1,30 @@
 package org.example.project.core.local.storage
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.core.okio.OkioStorage
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.ForScope
 import dev.zacsweers.metro.Provides
-import dev.zacsweers.metro.SingleIn
 import java.io.File
 import kotlinx.serialization.ExperimentalSerializationApi
 import okio.FileSystem
 import okio.Path.Companion.toOkioPath
 import org.example.project.core.local.storage.SettingsLocalDataSourceImpl.Companion.SETTINGS_FILE_NAME
+import org.example.project.core.local.storage.di.DataStoreScope
 import org.example.project.core.local.storage.model.SettingsLocalModel
 
 @ContributesTo(AppScope::class)
-interface DataStoreGraph {
+interface JvmDataStoreStorageGraph {
     @OptIn(ExperimentalSerializationApi::class)
     @Provides
-    @SingleIn(AppScope::class)
-    fun provideDataStore(): DataStore<SettingsLocalModel> =
-        DataStoreFactory.create(
-            storage =
-                OkioStorage(
-                    fileSystem = FileSystem.SYSTEM,
-                    serializer = dataStoreJsonSerializer(SettingsLocalModel()),
-                    producePath = {
-                        // TODO: Store data in a persistent OS dependent dir
-                        File(System.getProperty("java.io.tmpdir"), SETTINGS_FILE_NAME).toOkioPath()
-                    },
-                ),
-            corruptionHandler = ReplaceFileCorruptionHandler { SettingsLocalModel() },
+    @ForScope(DataStoreScope::class)
+    fun provideStorage(): OkioStorage<SettingsLocalModel> =
+        OkioStorage(
+            fileSystem = FileSystem.SYSTEM,
+            serializer = dataStoreJsonSerializer(SettingsLocalModel()),
+            producePath = {
+                // TODO: Store data in a persistent OS dependent dir
+                File(System.getProperty("java.io.tmpdir"), SETTINGS_FILE_NAME).toOkioPath()
+            },
         )
 }
