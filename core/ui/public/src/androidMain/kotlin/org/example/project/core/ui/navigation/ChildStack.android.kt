5a54c7175a076d963afcbaf1ebbabf4ea1aa3df3
Nazar Rusnak <inazarrusnak@gmail.com>
2026-03-17 19:00:42 +0200

    Fix formatting

--- core/ui/public/src/androidMain/kotlin/org/example/project/core/ui/navigation/ChildStack.android.kt
+++ core/ui/public/src/androidMain/kotlin/org/example/project/core/ui/navigation/ChildStack.android.kt
@@ -77,20 +77,19 @@ private class MaterialSharedAxisXAnimatable(initialEvent: BackEvent) : Predictiv
     private val finishAnimatable = Animatable(0f)
     private val progressAnimatable = Animatable(initialEvent.progress)
 
-    override val exitModifier: Modifier =
-        Modifier.graphicsLayer {
-            val progress = progressAnimatable.value
-            val finishProgress = finishAnimatable.value
+    override val exitModifier: Modifier = Modifier.graphicsLayer {
+        val progress = progressAnimatable.value
+        val finishProgress = finishAnimatable.value
 
-            translationX =
-                lerp(
-                    start = size.width * MoveFactor * progress,
-                    stop = size.width,
-                    fraction = finishProgress,
-                )
-            alpha = 1f - finishProgress
-            compositingStrategy = CompositingStrategy.Offscreen
-        }
+        translationX =
+            lerp(
+                start = size.width * MoveFactor * progress,
+                stop = size.width,
+                fraction = finishProgress,
+            )
+        alpha = 1f - finishProgress
+        compositingStrategy = CompositingStrategy.Offscreen
+    }
 
     override val enterModifier: Modifier =
         Modifier.drawWithContent {
