96efa14a788edbe6562c216b8d5a8e0a5e3c3632
HarukeyUA <rainymail@proton.me>
2026-04-14 18:11:19 +0300

    Refactor event collection in MoleculeComponent
    - Use `LaunchedEffect` scope directly in `CollectEvents` instead of `rememberCoroutineScope` for launching event handlers

--- core/component/public/src/commonMain/kotlin/org/example/project/core/component/MoleculeComponent.kt
+++ core/component/public/src/commonMain/kotlin/org/example/project/core/component/MoleculeComponent.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import app.cash.molecule.RecompositionMode
@@ -65,10 +64,9 @@ abstract class MoleculeComponent<S : UiState, E : UiEvent>(componentContext: App
      */
     @Composable
     protected fun CollectEvents(onEvent: suspend (E) -> Unit) {
-        val coroutineScope = rememberCoroutineScope()
         val onEventUpdatedState by rememberUpdatedState(onEvent)
         LaunchedEffect(Unit) {
-            events.consumeAsFlow().collect { coroutineScope.launch { onEventUpdatedState(it) } }
+            events.consumeAsFlow().collect { launch { onEventUpdatedState(it) } }
         }
     }
 
